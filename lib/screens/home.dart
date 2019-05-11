ed41d547eea61858995299051ed8c8ab900905da
masterUNG <phrombutr@gmail.com>
2019-05-11 15:08:21 +0700

    Drawer OK

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,12 +6,69 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String title1 = 'มาสเตอร์ อึ่ง';
+  String title2 = 'www.androidthai.in.th';
+
+  Widget showTitle() {
+    return Text(
+      title1,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 18.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget showSubTitle() {
+    return Text(title2);
+  }
+
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
+  }
+
+  Widget menuDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.green[300]),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 80.0,
+                    width: 100.0,
+                    child: showLogo(),
+                  ),
+                  showTitle(),
+                  showSubTitle()
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home, color: Colors.green[900],),
+            title: Text('Home', style: TextStyle(fontSize: 18.0),),
+          ),
+          ListTile(
+            leading: Icon(Icons.android, color: Colors.green[900],),
+            title: Text('แอนดรอยด์', style: TextStyle(fontSize: 18.0),),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello Home'),
-      ),body: Text('This is body'),
+        title: showTitle(),
+      ),
+      body: Text('This is body'),
+      drawer: menuDrawer(context),
     );
   }
 }
